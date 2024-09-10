27dfd257725a0dda8056b63186614b7530bec13e
ton super parrain <hirtouwatche@gmail.com>
2024-09-10 20:35:19 +0200

    corrections to initial commit

--- script.js
+++ script.js
@@ -1,11 +1,71 @@
-function verifierMotDePasse() {
-    const motDePasse = document.getElementById("motDePasse").value;
-
-    if (motDePasse === "FIP") {
-        // Rediriger vers une autre page (remplacez "autre_page.html" par le nom de votre page)
-        window.location.href = "index2.html";
-    } else {
-        // Afficher un message d'erreur (optionnel)
-        alert("Mot de passe incorrect");
+function _0x4a55() {
+  const _0x5dea36 = [
+    "11aXxxpo",
+    "13086oeTWoS",
+    "location",
+    "21LjIZyE",
+    "826QmTvYm",
+    "href",
+    "FIP",
+    "18546384qMPVHj",
+    "432528lVxrix",
+    "value",
+    "Mot\x20de\x20passe\x20incorrect",
+    "getElementById",
+    "285892PlkvwP",
+    "index2.html",
+    "533944aOAsIh",
+    "4970ijRjCA",
+    "2488602NVKVAo",
+    "motDePasse",
+    "5hPfOii",
+    "34912SuVveK",
+  ];
+  _0x4a55 = function () {
+    return _0x5dea36;
+  };
+  return _0x4a55();
+}
+function _0x3ad5(_0xfed933, _0x121963) {
+  const _0x4a55ad = _0x4a55();
+  return (
+    (_0x3ad5 = function (_0x3ad54d, _0x345e31) {
+      _0x3ad54d = _0x3ad54d - 0x167;
+      let _0x112d84 = _0x4a55ad[_0x3ad54d];
+      return _0x112d84;
+    }),
+    _0x3ad5(_0xfed933, _0x121963)
+  );
+}
+(function (_0x3247ae, _0x54ffba) {
+  const _0x4fae70 = _0x3ad5,
+    _0xedaefc = _0x3247ae();
+  while (!![]) {
+    try {
+      const _0x22527a =
+        -parseInt(_0x4fae70(0x16c)) / 0x1 +
+        -parseInt(_0x4fae70(0x168)) / 0x2 +
+        (parseInt(_0x4fae70(0x177)) / 0x3) *
+          (parseInt(_0x4fae70(0x16e)) / 0x4) +
+        (-parseInt(_0x4fae70(0x172)) / 0x5) *
+          (-parseInt(_0x4fae70(0x170)) / 0x6) +
+        (-parseInt(_0x4fae70(0x178)) / 0x7) *
+          (-parseInt(_0x4fae70(0x173)) / 0x8) +
+        (-parseInt(_0x4fae70(0x175)) / 0x9) *
+          (-parseInt(_0x4fae70(0x16f)) / 0xa) +
+        (parseInt(_0x4fae70(0x174)) / 0xb) *
+          (-parseInt(_0x4fae70(0x167)) / 0xc);
+      if (_0x22527a === _0x54ffba) break;
+      else _0xedaefc["push"](_0xedaefc["shift"]());
+    } catch (_0x224f3c) {
+      _0xedaefc["push"](_0xedaefc["shift"]());
     }
+  }
+})(_0x4a55, 0x839bf);
+function verifierMotDePasse() {
+  const _0x1e625a = _0x3ad5,
+    _0x215547 = document[_0x1e625a(0x16b)](_0x1e625a(0x171))[_0x1e625a(0x169)];
+  _0x215547 === _0x1e625a(0x17a)
+    ? (window[_0x1e625a(0x176)][_0x1e625a(0x179)] = _0x1e625a(0x16d))
+    : alert(_0x1e625a(0x16a));
 }
